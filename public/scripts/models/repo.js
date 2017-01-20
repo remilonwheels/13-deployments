33a620b1bad95b044f744274d8bff936b160ddb0
Remil Marzan <remilonwheels@gmail.com>
2017-01-19 20:22:27 -0800

    ajax update

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -7,13 +7,7 @@
 
   repos.requestRepos = function(callback) {
     // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
-    $.ajax({
-      url: `https://api.github.com/user/repos
-            ?per_page=5
-            &sort=updated`,
-      type: 'GET',
-      headers: {'Authorization': `token ${githubToken}`}
-    })
+    $.get('/github/user/repos?per_page=5&sort=updated')
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
   };
