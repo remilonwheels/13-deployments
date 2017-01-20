37f62b7ef8c26e54d481eddcba840aa13f438482
Remil Marzan <remilonwheels@gmail.com>
2017-01-19 20:44:51 -0800

    marked todo done

--- public/scripts/models/repo.js
+++ public/scripts/models/repo.js
@@ -6,7 +6,7 @@
   repos.all = [];
 
   repos.requestRepos = function(callback) {
-    // TODO: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
+    // DONE: Refactor your ajax call to use the $.get method, and make a request to our new proxy route.
     $.get('/github/user/repos?per_page=5&sort=updated')
     .then(data => repos.all = data, err => console.error(err)) // es6 syntax arrow functions
     .then(callback);
